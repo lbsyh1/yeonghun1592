15173e93be10108d5b9a10c0cc37f1cfa2cbefc2
lbsyh1 <84220897+lbsyh1@users.noreply.github.com>
2022-08-31 14:43:15 +0900

    Update Home.js

--- projects/Movie App/routes/Home.js
+++ projects/Movie App/routes/Home.js
@@ -41,3 +41,17 @@ function Home() {
 export default Home;
 
 ```
+
+```
+
+ const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+```
